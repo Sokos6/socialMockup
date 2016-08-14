cb92d94733d0d3de22b494129db96dd68dd158ee
Sokos6 <will@comicbooked.com>
2016-08-13 20:16:04 -0400

    input field binding for status updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
+// create our app module
 var app = angular.module('app', [])
+
+// create the PostsCtrl module
+// dependency inject $scope
 app.controller('PostsCtrl', function ($scope) {
+
+    // the function runs when the "Add Post" button is clicked
+    $scope.addPost = function () {
+        // Only add a post if there is a body
+        if ($scope.postBody) {
+            // unshift a new post into $scope.posts
+            $scope.posts.unshift({
+                username: 'sokos6',
+                body: $scope.postBody // use the text entered
+            })
+            // clear out the input field
+            $scope.postBody = null
+        }
+    }
+
+    // starting data
     $scope.posts = [
         {
             username: 'sokos6',
@@ -10,11 +30,4 @@ app.controller('PostsCtrl', function ($scope) {
             body: 'we love angular.js...'
         }
     ]
-
-    $scope.addPost = function() {
-        $scope.posts.unshift({
-            username: 'sokos6',
-            body: 'my new post!'
-        })
-    }
-})
+});

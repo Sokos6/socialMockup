615a2f14c9d6084d38640d66114357dabd214e95
Sokos6 <will@comicbooked.com>
2016-08-13 20:39:43 -0400

    node server and rest api created

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/api/posts', function (req, res) {
+    res.json([
+        {
+            username: 'sokos6' +
+            '' +
+            '',
+            body: 'node rocks!'
+        }
+    ])
+})
+
+app.listen(3000, function () {
+    console.log('Server listening on', 3000)
+})
+
+app.post('/api/posts', function (req, res) {
+    console.log('post received!')
+    console.log(req.body.username)
+    console.log(req.body.body)
+    res.send(201)
+})
